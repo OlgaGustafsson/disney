63180316f2fbb9e0da757a480982be188661f0d0
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2023-02-01 21:17:32 +0100

    update

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,63 +1,92 @@
-/* From API https://api.disneyapi.dev/character */
+/* From API https://api.disneyapi.dev */
 
 
-const url = "https://api.disneyapi.dev/character";
-const urlSearch = "https://api.disneyapi.dev/characters";
-//const urlSearch = "https://api.disneyapi.dev/character/search?query="
-//const urlName = "https://api.disneyapi.dev/character?name="
+const url = "https://api.disneyapi.dev/characters";
 
 const imgElem = document.querySelector('#img') as HTMLImageElement;
 //const imageSec = document.querySelector("#image") as HTMLElement;
 
-const buttonSearch = document.querySelector('#search-btn') as HTMLButtonElement;
-const inputNumber = document.querySelector("#id-number") as HTMLInputElement;
-//const infoSec = document.querySelector('#info') as HTMLElement;
+let button = document.querySelector("#button-image") as HTMLButtonElement;
+// const buttonSearch = document.querySelector('#search-btn') as HTMLButtonElement;
+// const inputNumber = document.querySelector("#id-number") as HTMLInputElement;
+const infoSec = document.querySelector('#info') as HTMLElement;
 
 
-//______________show character/ random______________
 
-const char = fetch(url)
-    .then(response => {
-        return response.json();
-    })
-    .then(charsArray => {
-        printChar(charsArray);
+//____________get all characters__________
 
-        console.log(charsArray);
-    });
 
-function printChar(chars: any) {
-    let button = document.querySelector("#button") as HTMLButtonElement;
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
+getAllCharacters();
+async function getAllCharacters() {
+    //button.addEventListener("click", async (event) => {
+        //event.preventDefault();
+        const response = await fetch(url);
+        const data = await response.json();
 
-        let randomN = Math.floor(Math.random() * chars.length + 1);
-         
-        
-        imgElem.src = chars[randomN].imageUrl;
+        console.log(data);
+    
+        //const randomElem = Math.floor(Math.random() * data.length + 1);
 
-    });
+        //imgElem.src = randomElem.imageUrl;
+        //console.log(randomElem.imageUrl);
+    //})
 };
 
-console.log(("hej"));
+//_______show character/ random__________
+
+button.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    async function showImage() {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const arrlength = data.length;
+        const randomElem = Math.floor(Math.random() * arrlength + 1);
+
+        imgElem.src = data[randomElem].imageUrl;
+
+        console.log(data[randomElem].imageUrl);
+
+        // const randomElem = Math.floor(Math.random() * data.length + 1);
+        
+        // imgElem.src = randomElem.imageUrl;
+        // console.log(randomElem.imageUrl);
+
+        
+	//     let length: number = data.length;
+    //     for (let i = 0; i < length; i++) {
+    //         const infoDiv = document.createElement(".info") as HTMLDivElement;
+    //         infoDiv.innerHTML = "";
+    //         infoDiv.innerHTML = `Hi! My name is ${data.results[i].name}`;
+    //         infoSec.append(infoDiv);
+            
+    //         console.log(data.results[i].name);
+    // };
+    
+}
+showImage();
+
+});
+
+
+
+
 
 
 //________________search character by id___________________
 
 
-buttonSearch.addEventListener("click", async (event) => {
-    event.preventDefault();
+// buttonSearch.addEventListener("click", async (event) => {
+//     event.preventDefault();
 
-    const response = await fetch(urlSearch + inputNumber.value);
-    const data = await response.json();
+//     const response = await fetch(urlSearch + inputNumber.value);
+//     const data = await response.json();
 
     // const elementById = data.results.url;
     // imgElem.src = elementById;
-    
-
-   
-   
-});
+      
+// });
 
 
 
